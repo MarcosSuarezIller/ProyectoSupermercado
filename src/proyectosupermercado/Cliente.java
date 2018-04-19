ad8e80685562e425094a2006491d5b11cd3f21fd
DAM106 <DAM106@IF02-16.iesmhp.local>
2018-04-19 16:11:54 +0200

    Creado clase cliente

--- src/proyectosupermercado/Cliente.java
+++ src/proyectosupermercado/Cliente.java
@@ -12,4 +12,64 @@ package proyectosupermercado;
  */
 public class Cliente {
     
+    private int numeroCliente;
+    private String nombre;
+    private String telefono;
+    private String direccion;
+
+    public Cliente() {
+    }
+
+    public Cliente(int numeroCliente, String nombre, String telefono, String direccion) {
+        this.numeroCliente = numeroCliente;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+    
+    public Cliente(Cliente c) {
+        this.numeroCliente = c.getNumeroCliente();
+        this.nombre = c.getNombre();
+        this.telefono = c.getTelefono();
+        this.direccion = c.getDireccion();
+    }
+
+    public int getNumeroCliente() {
+        return numeroCliente;
+    }
+
+    public void setNumeroCliente(int numeroCliente) {
+        this.numeroCliente = numeroCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "numeroCliente=" + numeroCliente + ", nombre=" + nombre + ", telefono=" + telefono + ", direccion=" + direccion + '}';
+    }
+    
+    
 }
